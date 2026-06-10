733353197c48fc498a59fec8c92dc464cb75046d
abdulm3523 <abdulmalik3523@gmail.com>
2026-06-10 13:14:54 +0300

    Start movie app practice project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import Boimela from "./Boimela";
 
 function App() {
   return (
-    <div className="max-w-5xl mx-auto p-4 md:p-6">
-      <Boimela />
+    <div class="max-w-5xl mx-auto p-4 md:p-6">
+      <h2>Hello</h2>
     </div>
   );
 }
--- src/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-  return <h1 className="text-4xl font-bold text-black mb-6">Boimela</h1>;
-}
-
-export default Header;
